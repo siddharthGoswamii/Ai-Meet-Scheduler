413c5a600027289a49bc01ad153746e671b49588
Siddharth Goswami <sidgoswami9891@gmail.com>
2026-05-27 18:27:14 +0530

    Auth fixed

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -42,6 +42,7 @@ class AuthService:
             "https://www.googleapis.com/auth/calendar.readonly",
             "https://www.googleapis.com/auth/userinfo.email",
             "https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/contacts.readonly",
         ]
 
         # Encryption
