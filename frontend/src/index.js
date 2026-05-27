39ca890525facf89f149755a3a544cdc740c0528
Siddharth Goswami <sidgoswami9891@gmail.com>
2026-05-27 18:23:11 +0530

    Favicon updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
@@ -15,4 +21,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-document.getElementById('app-loader').style.display = 'none';
+
+const appLoader = document.getElementById('app-loader');
+if (appLoader) {
+  appLoader.style.display = 'none';
+}
